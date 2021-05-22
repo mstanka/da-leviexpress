ce0e3247c4628e2fd7b2658dd73f3ac5c0f75189
mstanka <mice78.mm@gmail.com>
2021-05-22 10:47:21 +0200

    add router with home and reservation page

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Reservation from './pages/Reservation';
 import './style.css';
 
 const App = () => (
-  <div className="container">
-    <Header />
-    <main>Main</main>
-    <Footer />
-  </div>
+  <Router>
+    <div className="container">
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/reservation">
+            <Reservation />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
+    </div>
+  </Router>
 );
 
 render(<App />, document.querySelector('#app'));
---
+++ src/pages/Reservation/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reservation = () => {
+  return <h2>Reservation</h2>;
+};
+
+export default Reservation;
