e7f41f7125ba304783ee032c594e273b666faaa5
mstanka <mice78.mm@gmail.com>
2021-05-22 10:24:21 +0200

    add header and styles

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1 className="site-title">
+          <a href="/">LeviExpress</a>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
