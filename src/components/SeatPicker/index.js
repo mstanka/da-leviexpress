0adcd53473f7111009c220c5006cded54216918a
mstanka <mice78.mm@gmail.com>
2021-05-23 09:23:44 +0200

    update responsive styles

--- src/components/SeatPicker/index.js
+++ src/components/SeatPicker/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Seat from './Seat';
+import './style.css'
 
 const SeatPicker = ({ seats, journeyId }) => {
   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
